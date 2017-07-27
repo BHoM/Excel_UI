8bcfe0f73a2f611a8161dba9e78c12fb07d07169
IsakNaslund <isak.naslund@burohappold.com>
2017-07-27 09:30:30 +0200

    From mongo to table form in excel

--- Mongo_Dragon/MongoLink.cs
+++ Mongo_Dragon/MongoLink.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,28 +44,163 @@ namespace Mongo_Dragon
         }
 
         /*****************************************************************/
+        [ExcelFunction(Description = "Test function", Category = "Mongo_Dragon")]
+        public static string ToMongoDictionary(
+            [ExcelArgument(Name = "objects")] object[,] objects,
+            [ExcelArgument(Name = "key")] string key,
+            [ExcelArgument(Name = "server address")] string server,
+            [ExcelArgument(Name = "database name")] string database,
+            [ExcelArgument(Name = "collection name")] string collection)
+        {
+
+            string json = "{";
+
+            for (int i = 0; i < objects.GetLength(0); i++)
+            {
+                for (int j = 0; j < objects.GetLength(1); j++)
+                {
+
+                }
+            }
+
+            return "ToMongo";
+        }
+
+        private static string ItemToMongo(object o)
+        {
+            Guid guid;
+            BHB.BHoMObject bhO;
+            if (Guid.TryParse(o.ToString(), out guid) && (bhO = BHG.Project.ActiveProject.GetObject(guid)) != null)
+            {
+                return BHB.JSONWriter.Write(bhO);
+            }
+            else
+            {
+                return BHB.JSONWriter.Write(o);
+            }
+        }
+
+        /*****************************************************************/
 
         [ExcelFunction(Description = "Test function", Category = "Mongo_Dragon")]
         public static object FromMongo(
-            [ExcelArgument(Name = "query")] string query,
+            [ExcelArgument(Name = "query")] object[] query,
             [ExcelArgument(Name = "server address")] string server,
             [ExcelArgument(Name = "database name")] string database,
             [ExcelArgument(Name = "collection name")] string collection)
         {
             MA.MongoLink link = new MA.MongoLink(server, database, collection);
-            IEnumerable<object> objects = link.Pull(query);
+            IEnumerable<object> objects = link.Query(query.Select(x => x.ToString()).ToList());
 
-            object[] array = objects.Select(x => (x as BHB.BHoMObject).BHoM_Guid.ToString()).ToArray();
-            return XlCall.Excel(XlCall.xlUDF, "Resize", array);
+            int count = objects.Count();
+
+            //Check if object is dictionary
+            if (objects.FirstOrDefault() is IDictionary && count == 1)
+            {
+                IDictionary dict = objects.First() as IDictionary;
+                int colCount = dict.Keys.Count;
+                int rowCount = 2;
+
+                foreach (var key in dict.Keys)
+                {
+                    if (dict[key] is IList)
+                        rowCount = Math.Max(rowCount, (dict[key] as IList).Count + 1);
+                }
+
+                var array = new object[rowCount, colCount];
+
+                int n = 0;
+
+                foreach (var key in dict.Keys)
+                {
+                    array[0, n] = key.ToString();
+                    int m;
+                    if (dict[key] is IList)
+                    {
+                        IList list = dict[key] as IList;
+                        m = list.Count+1;
+                        for (int i = 0; i < m-1; i++)
+                        {
+                            array[i + 1, n] = ItemFromMongo(list[i]);
+                        }
+
+                    }
+                    else
+                    {
+                        array[1, n] = ItemFromMongo(dict[key]);
+                        m = 2;
+                    }
+                    for (int i = m; i < rowCount; i++)
+                    {
+                        array[i, n] = "";
+                    }
+
+                    n++;
+                }
+
+
+                return XlCall.Excel(XlCall.xlUDF, "Resize", array);
+            }
+            else if (count == 1)
+            {
+                return ItemFromMongo(objects.First());
+            }
+            else
+            {
+                object[] array = objects.Select(x => ItemFromMongo(x)).ToArray();
+                return XlCall.Excel(XlCall.xlUDF, "Resize", array);
+            }
+
+
         }
+
+
+        /*****************************************************************/
+
+
+        private static object ItemFromMongo(object o)
+        {
+            double d;
+            if (o == null)
+            {
+                return "Null Item";
+            }
+            if (o is BHB.BHoMObject)
+            {
+                BHB.BHoMObject bhO = o as BHB.BHoMObject;
+                BHG.Project.ActiveProject.AddObject(bhO);
+                return bhO.BHoM_Guid.ToString();
+            }
+            else if (double.TryParse(o.ToString(), out d))
+            {
+                return d;
+            }
+            else
+            {
+                return o.ToString();
+            }
+        }
+
+
+
+
+
+
+
+
 
         /*****************************************************************/
 
         [ExcelFunction(Description = "Test Array 2", Category = "Mongo_Dragon")]
         public static object TestArray2()
         {
-            object[,] array = new object[,] { { 3.4, 8.9 }, { "Mongo", "rules" } };
+            object[,] array = new object[,] { { 3.4, 8.9 }, { "Mongo", "rules" }, { "super duper", "Test" } };
             return XlCall.Excel(XlCall.xlUDF, "Resize", array);
         }
+
+        /****************************************************************/
+
+       
+
     }
 }
---
+++ StructuralDesign_Dragon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StructuralDesign_Dragon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Buro Happold")]
+[assembly: AssemblyProduct("StructuralDesign_Dragon")]
+[assembly: AssemblyCopyright("Copyright © Buro Happold 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1230ce1d-e590-4970-bd5e-bac5daca7039")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
