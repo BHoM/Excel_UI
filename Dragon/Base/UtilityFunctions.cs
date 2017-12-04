118c9079d450aa3e010820bcc5e76eef996067f4
INaslund <isak.naslund@burohappold.com>
2017-12-04 10:42:29 +0100

    Compiling with 2.0 and construction of objects

--- Dragon/Base/UtilityFunctions.cs
+++ Dragon/Base/UtilityFunctions.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExcelDna.Integration;
-using BHB = BHoM.Base;
-using BHG = BHoM.Global;
-using System.Reflection;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using ExcelDna.Integration;
+//using BHB = BHoM.Base;
+//using BHG = BHoM.Global;
+//using System.Reflection;
 
-namespace Dragon.Base
-{
-    public static class UtilityFunctions
-    {
-        [ExcelFunction(Description = "Create a BHoM object", Category = "Dragon")]
-        public static object StringFormat(
-            [ExcelArgument(Name = "format")] string format,
-            [ExcelArgument(Name = "args")] params object[] args)
-        {
+//namespace Dragon.Base
+//{
+//    public static class UtilityFunctions
+//    {
+//        [ExcelFunction(Description = "Create a BHoM object", Category = "Dragon")]
+//        public static object StringFormat(
+//            [ExcelArgument(Name = "format")] string format,
+//            [ExcelArgument(Name = "args")] params object[] args)
+//        {
 
-            return string.Format(format, args);
-        }
+//            return string.Format(format, args);
+//        }
 
-    }
-}
+//    }
+//}
