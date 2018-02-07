b293b80921a51fd338f82bed2c853b68b4ec79ef
INaslund <isak.naslund@burohappold.com>
2018-02-07 16:30:48 +0100

    Generic method calls + Geometry
    -Ability to call any method in the BHoM_Enginge implemented
    -Geometry support added. Still not on the fully same level as the objects due to lack of ID on the objects themself
    -All BHoM dlls copied to the BHoMFolder in app data
    -All dlls loaded upon start of excel

---
+++ Dragon/IO/InOutHelp.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using System.Reflection;
+using BH.oM.Base;
+using BH.Engine.Reflection;
+using BH.oM.Geometry;
+
+namespace BH.UI.Dragon
+{
+    public static class InOutHelp
+    {
+        /*****************************************************************/
+
+        public static object CheckAndGetObjectOrGeometry(this object obj)
+        {
+            Guid guid;
+            if (obj is string && Guid.TryParse(obj as string, out guid))
+            {
+                //Get out object or geometry
+                return Project.ActiveProject.GetAny(guid);
+            }
+            else
+            {
+                return obj;
+            }
+        }
+
+        /*****************************************************************/
+
+        public static object CheckAndGetObject(this object obj)
+        {
+            Guid guid;
+            if (obj is string && Guid.TryParse(obj as string, out guid))
+            {
+                //Get out object or geometry
+                return Project.ActiveProject.GetObject(guid);
+            }
+            else
+            {
+                return obj;
+            }
+        }
+
+        /*****************************************************************/
+
+        public static object CheckAndGetGeometry(this object obj)
+        {
+            Guid guid;
+            if (obj is string && Guid.TryParse(obj as string, out guid))
+            {
+                //Get out object or geometry
+                return Project.ActiveProject.GetGeometry(guid);
+            }
+            else
+            {
+                return obj;
+            }
+        }
+
+        /*****************************************************************/
+        public static bool SetPropertyHelper(IObject obj, object[] propNames, object[] propValues, out string message)
+        {
+            message = "";
+            for (int i = 0; i < propNames.Length; i++)
+            {
+                if ((propNames[i] is ExcelMissing) || (propValues[i] is ExcelMissing))
+                    continue;
+
+                object val = propValues[i].CheckAndGetObjectOrGeometry();
+                //Set the properties
+                if (!obj.SetPropertyValue(propNames[i] as string, val))
+                {
+                    message = propNames[i].ToString() + " is not a valid property for the type";
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /*****************************************************************/
+
+        public static bool SetPropertyHelper(CustomObject obj, object[] propNames, object[] propValues, out string message)
+        {
+            message = "";
+            for (int i = 0; i < propNames.Length; i++)
+            {
+                if ((propNames[i] is ExcelMissing) || (propValues[i] is ExcelMissing))
+                    continue;
+
+                object val = propValues[i].CheckAndGetObjectOrGeometry();
+                //Set the properties
+                if (propNames[i] as string == "Name")
+                {
+                    if (!obj.SetPropertyValue(propNames[i] as string, val))
+                    {
+                        message = propNames[i].ToString() + " is not a valid property for the type";
+                        return false;
+                    }
+                }
+                else
+                {
+                    obj.CustomData[propNames[i] as string] = val;
+                }
+            }
+            return true;
+        }
+
+
+        /*****************************************************************/
+
+        public static bool SetPropertyHelper(IBHoMGeometry obj, object[] propNames, object[] propValues, out string message)
+        {
+            message = "";
+            for (int i = 0; i < propNames.Length; i++)
+            {
+                if ((propNames[i] is ExcelMissing) || (propValues[i] is ExcelMissing))
+                    continue;
+
+                object val = propValues[i].CheckAndGetGeometry();
+
+                if (!obj.SetPropertyValue(propNames[i] as string, val))
+                {
+                    message = propNames[i].ToString() + " is not a valid property for the type";
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /*****************************************************************/
+
+        public static object ReturnTypeHelper(this object obj)
+        {
+            if (obj == null)
+                return "Failed to get property";
+            else if (obj is IObject)
+            {
+                IObject iObj = (IObject)obj;
+                Project.ActiveProject.AddObject(iObj);
+                return iObj.BHoM_Guid.ToString();
+            }
+            else if (obj is IBHoMGeometry)
+            {
+                return Project.ActiveProject.AddGeometry(obj as IBHoMGeometry).ToString();
+            }
+            else if (IsNumeric(obj))
+                return obj;
+
+            return obj.ToString();
+        }
+
+        /*****************************************************************/
+
+        public static bool IsNumeric(object obj)
+        {
+            if (obj is double)
+                return true;
+            if (obj is int)
+                return true;
+            if (obj is float)
+                return true;
+            if (obj is decimal)
+                return true;
+            if (obj is byte)
+                return true;
+
+            return false;
+        }
+
+        /*****************************************************************/
+    }
+}
---
+++ Dragon/Methods/GenericMethodCall.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using System.Reflection;
+using BH.oM.Base;
+using BH.Engine.Reflection;
+
+namespace BH.UI.Dragon
+{
+    public static class GenericMethodCall
+    {
+        /*****************************************************************/
+        /******* Public methods                             **************/
+        /*****************************************************************/
+
+        [ExcelFunction(Description = "Get the names of all the types of Methods", Category = "Dragon")]
+        public static object GetAllMethods(
+            [ExcelArgument(Name = "Optional type of method delcaration")] string type = null)
+        {
+
+            string[] methodNames;
+            if (string.IsNullOrWhiteSpace(type))
+                methodNames = Query.BHoMMethodList().Select(x => x.Name).ToArray();
+            else
+                methodNames = Query.BHoMMethodList().Where(x => x.DeclaringType.Name == type).Select(x => x.Name).ToArray();
+
+
+            return XlCall.Excel(XlCall.xlUDF, "Resize", methodNames);
+        }
+
+        /*****************************************************************/
+
+        [ExcelFunction(Description = "Calls a method with a specific name and set of arguments", Category = "Dragon")]
+        public static object CallMethod(
+                [ExcelArgument(Name = "MethodName")] string methodName,
+               [ExcelArgument(Name = "Arguments")] object[] arguments )
+        {
+
+            List<MethodInfo> methods = Query.BHoMMethodList().Where(x => x.Name == methodName).ToList();
+
+            if (methods.Count == 0)
+                return "Method with the given name not found";
+
+            arguments = arguments.Select(x => x.CheckAndGetObjectOrGeometry()).ToArray();
+
+            object[] matchingArgs;
+            MethodInfo method;
+            if (!MatchMethodAndAguments(methods, arguments, out matchingArgs, out method))
+                return "Method matching the provided arguments not found";
+
+
+            object result = null;
+            try
+            {
+                result = method.Invoke(null, matchingArgs);
+            }
+            catch (Exception e)
+            {
+                return "Method call failed. Inner exception message: " + e.Message;
+            }
+
+            if (result is IEnumerable)
+            {
+                object[] arr = (result as IEnumerable<object>).Select(x => x.ReturnTypeHelper()).ToArray();
+                return XlCall.Excel(XlCall.xlUDF, "Resize", arr);
+            }
+            else
+                return result.ReturnTypeHelper();
+            
+        }
+
+        /*****************************************************************/
+
+        private static bool MatchMethodAndAguments(List<MethodInfo> methods, object[] arguments, out object[] matchingArgs, out MethodInfo method)
+        {
+            bool found = false;
+            matchingArgs = null;
+            int argCount = arguments.Length;
+            method = null;
+            //Loop through all methods
+            foreach (MethodInfo info in methods)
+            {
+                ParameterInfo[] paramInfo = info.GetParameters();
+                bool matching = true;
+                matchingArgs = new object[paramInfo.Length];
+
+                //Loop trhough all the arguments of the method
+                for (int i = 0; i < paramInfo.Length; i++)
+                {
+                    if (i < argCount)
+                    {
+                        //Check of the parametertype matches the expected
+                        if (paramInfo[i].ParameterType == arguments[i].GetType())
+                            matchingArgs[i] = arguments[i];
+                        else if (arguments[i] == ExcelMissing.Value && paramInfo[i].IsOptional) //Check for empty cells
+                            matchingArgs[i] = paramInfo[i].RawDefaultValue;
+                        else
+                        {
+                            matching = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        //Check if parameter is optional
+                        if (paramInfo[i].IsOptional)
+                        {
+                            matchingArgs[i] = paramInfo[i].RawDefaultValue;
+                        }
+                        else
+                        {
+                            matching = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (matching)
+                {
+                    found = true;
+                    method = info;
+                    break;
+                }
+
+            }
+
+            return found;
+        }
+
+        /*****************************************************************/
+    }
+}
