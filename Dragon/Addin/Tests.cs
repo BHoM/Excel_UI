c942433f57d6bd855b61a1a7fdcefa7367023dbf
IsakNaslund <isak.naslund@burohappold.com>
2018-02-13 09:54:25 +0100

    Cleanup and tests

---
+++ Dragon/Addin/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structural.Loads;
+using BH.oM.Structural.Elements;
+using BH.oM.Geometry;
+
+namespace BH.UI.Dragon
+{
+    public static class Tests
+    {
+
+        public static string TestEnumDefault(LoadNature nature = LoadNature.Dead)
+        {
+            return nature.ToString();
+        }
+
+        public static Node TestNode(Point point = null)
+        {
+            return point != null ? new Node() { Position = point } : new Node() { Position = new Point() { X = 1, Y = 2, Z = 3 } };
+        }
+    }
+}
