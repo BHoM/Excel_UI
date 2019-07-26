d6ef48f2faf7b4f5900b25691e679d500676cd98
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-07-26 09:37:52 +0100

    Add Engine method to set cell content

---
+++ Excel_Engine/Compute/Contents.cs
@@ -0,0 +1,28 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Excel
+{
+    public static partial class Compute
+    {
+        public static bool Contents(this oM.Excel.Reference reference, string value)
+        {
+            ExcelAsyncUtil.QueueAsMacro(() =>
+            {
+                try
+                {
+                    XlCall.Excel(XlCall.xlcFormula, value, reference.ToExcel());
+                }
+                catch (XlCallException exception)
+                {
+                    Reflection.Compute.RecordError(exception.Message);
+                }
+            });
+            return true;
+        }
+    }
+}
