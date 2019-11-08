7a36c581fa5e6cb5f6028771f1019112e3f609ff
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-11-08 17:36:04 +0000

    Add Engine Method to get filename

---
+++ Excel_Engine/Query/GetFilename.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BH.Engine.Excel
+{
+    public static partial class Query
+    {
+        public static string GetFilename()
+        {
+            string reftext = Caller().RefText();
+            return Regex.Match(reftext, @"\[(.*)\]").Groups[1].Value;
+        }
+    }
+}
