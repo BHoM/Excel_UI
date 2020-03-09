49b379f5fede658121b1b93657b02f3383d6a9e2
Andrew Wakeman <andrew.wakeman@burohappold.com>
2020-03-09 09:00:08 +0000

    Add expression parsing

---
+++ Excel_oM/Expressions/BinaryExpression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Excel.Expressions
+{
+    public class BinaryExpression : IExpression
+    {
+        public string Operator { get; set; }
+        public IExpression Left { get; set; }
+        public IExpression Right { get; set; }
+    }
+}
---
+++ Excel_oM/Expressions/UnaryExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Excel.Expressions
+{
+    public class UnaryExpression : IExpression
+    {
+        public string Operator { get; set; }
+        public IExpression Expression { get; set; }
+    }
+}
