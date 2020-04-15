16bf0b83a68b4c94884289b89f808896500b46f1
Andrew Wakeman <andrew.wakeman@burohappold.com>
2020-04-15 15:57:43 +0100

    Load deprecated methods to allow them to work as formulas, still not exposed in global search

--- Excel_UI/UI/Global/FormulaSearchMenu.cs
+++ Excel_UI/UI/Global/FormulaSearchMenu.cs
@@ -20,8 +20,11 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
  */
 
+using BH.Adapter;
 using BH.Engine.Reflection;
+using BH.Engine.UI;
 using BH.oM.UI;
+using BH.UI.Components;
 using BH.UI.Excel.Callers;
 using BH.UI.Excel.Templates;
 using BH.UI.Global;
@@ -86,6 +89,68 @@ namespace BH.UI.Excel.Global
                 caller.EnqueueRegistration();
             }
         }
+
+        protected override List<SearchItem> GetAllPossibleItems()
+        {
+            var items = base.GetAllPossibleItems();
+
+            // All constructors for the BHoM objects
+
+            items.AddRange(Engine.Reflection.Query.BHoMTypeList()
+                .Where(x => !x.IsNotImplemented() && x.IsDeprecated() && x?.GetInterface("IImmutable") == null && !x.IsEnum && !x.IsAbstract)
+                .Select(x => new SearchItem { Item = x, CallerType = typeof(CreateObjectCaller), Text = x.ConstructorText() }));
+
+            // All methods for the BHoM Engine
+            items.AddRange(Engine.Reflection.Query.BHoMMethodList().Where(x=>x.IsDeprecated())
+                .Select(x => new SearchItem { Item = x, CallerType = GetCallerType(x), Icon = GetIcon(x), Text = x.ToText(true) }));
+
+            // All adapter constructors
+            items.AddRange(Engine.Reflection.Query.AdapterTypeList()
+                .Where(x => x.IsSubclassOf(typeof(BHoMAdapter)))
+                .SelectMany(x => x.GetConstructors())
+                .Where(x => !x.IsNotImplemented() && x.IsDeprecated())
+                .Select(x => new SearchItem { Item = x, CallerType = typeof(CreateAdapterCaller), Text = x.ToText(true) }));
+
+            // All Types
+            items.AddRange( Engine.Reflection.Query.BHoMTypeList()
+                .Concat(Engine.Reflection.Query.BHoMInterfaceList())
+                .Where(x => !x.IsNotImplemented() && x.IsDeprecated())
+                .Select(x => new SearchItem { Item = x, CallerType = typeof(CreateTypeCaller), Text = x.ToText(true) }));
+
+            // All Enums
+            items.AddRange(Engine.Reflection.Query.BHoMEnumList()
+                .Where(x => !x.IsNotImplemented() && x.IsDeprecated())
+                .Select(x => new SearchItem { Item = x, CallerType = typeof(CreateEnumCaller), Text = x.ToText(true) }));
+
+            return items;
+        }
+
+        /*************************************/
+
+        private static Type GetCallerType(MethodBase item)
+        {
+            if (item.DeclaringType.Namespace.StartsWith("BH.Engine"))
+            {
+                switch (item.DeclaringType.Name)
+                {
+                    case "Compute":
+                        return typeof(ComputeCaller);
+                    case "Convert":
+                        return typeof(ConvertCaller);
+                    case "Create":
+                        return typeof(CreateObjectCaller);
+                    case "Modify":
+                        return typeof(ModifyCaller);
+                    case "Query":
+                        return typeof(QueryCaller);
+                    default:
+                        return null;
+                }
+            }
+            else
+                return null;
+        }
+
 
         /*******************************************/
         /**** Private Fields                    ****/
