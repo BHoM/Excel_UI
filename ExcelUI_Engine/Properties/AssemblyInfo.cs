739160f9e9db06405e2bd039e82fd373fa0f8513
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-07-04 16:10:56 +0100

    Make assembly info compliant

--- ExcelUI_Engine/Properties/AssemblyInfo.cs
+++ ExcelUI_Engine/Properties/AssemblyInfo.cs
@@ -1,16 +1,38 @@
-﻿using System.Reflection;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *
+ *
+ * The BHoM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License, or
+ * (at your option) any later version.
+ *
+ * The BHoM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExcelUI_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Excel_UI")]
+[assembly: AssemblyDescription("Excel UI for the BHoM")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BuroHappold Engineering")]
-[assembly: AssemblyProduct("ExcelUI_Engine")]
-[assembly: AssemblyCopyright("Copyright © BuroHappold Engineering 2022")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BHoM")]
+[assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +42,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("56039ed6-fbd5-4f45-936b-2116399aeec3")]
+[assembly: Guid("0faf6feb-80c3-474a-92bb-c886424ccbc8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +54,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
+
+
+
