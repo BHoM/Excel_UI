77bc723580261e7b8dc8bd28c5302a76afc477bf
Desai Wang <desai.wang@burohappold.com>
2023-06-09 13:21:20 -0400

    Added ExcelUIWiki.cs

---
+++ ExcelUI_Engine/Query/ExcelUIWiki.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Reflection;
+using BH.oM.Base.Attributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Excel
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Generates the Excel UI wiki URL for a provided page.")]
+        [Input("page", "An optional page in the wiki to link to. If no page is provided, the root URL is returned.")]
+        [Output("url", "Fully qualified URL for the Excel UI wiki.")]
+        public static string ExcelUIWiki(string page = null)
+        {
+            string url = "https://github.com/BHoM/Excel_UI/wiki";
+
+            if (!string.IsNullOrEmpty(page))
+            {
+                url += $"/{page}";
+            }
+
+            return url;
+        }
+
+        /***************************************************/
+    }
+}
