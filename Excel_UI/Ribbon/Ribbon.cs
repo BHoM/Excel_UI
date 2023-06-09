64a0b4278a0dcaae2498e7b5c85eb4a85b800808
Desai Wang <desai.wang@burohappold.com>
2023-06-09 13:31:31 -0400

    Refactored dependencies

--- Excel_UI/Ribbon/Ribbon.cs
+++ Excel_UI/Ribbon/Ribbon.cs
@@ -20,7 +20,6 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
  */
 
-using BH.Engine.Excel;
 using BH.UI.Excel.Templates;
 using ExcelDna.Integration.CustomUI;
 using System;
@@ -28,8 +27,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace BH.UI.Excel.Addin
