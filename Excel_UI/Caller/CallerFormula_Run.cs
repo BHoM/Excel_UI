eb58520eaec9ca8add44590108f433924fc8dd97
IsakNaslundBh <isak.naslund@burohappold.com>
2025-02-14 08:48:11 +0100

    Correct setting project ID via workbook title, directly providing it to the log method

--- Excel_UI/Caller/CallerFormula_Run.cs
+++ Excel_UI/Caller/CallerFormula_Run.cs
@@ -58,8 +58,8 @@ namespace BH.UI.Excel.Templates
             // Log usage
             Application app = ExcelDnaUtil.Application as Application;
             Workbook workbook = app.ActiveWorkbook;
-            SetProjectID(workbook, AddIn.WorkbookId(workbook));
-            Engine.UI.Compute.LogUsage("Excel", app?.Version, InstanceId, Caller.GetType().Name, Caller.SelectedItem, Engine.Base.Query.CurrentEvents(), AddIn.WorkbookId(workbook), workbook.FullName);
+            //Grabbing projectId from workbook title
+            Engine.UI.Compute.LogUsage("Excel", app?.Version, InstanceId, Caller.GetType().Name, Caller.SelectedItem, Engine.Base.Query.CurrentEvents(), AddIn.WorkbookId(workbook), workbook.FullName, workbook.Title);
 
             // Return result
             return result;
@@ -84,20 +84,6 @@ namespace BH.UI.Excel.Templates
 
         /*******************************************/
 
-        private static void SetProjectID(Workbook workbook, string fileID)
-        {
-            string projectId = workbook.Title;
-
-            if (!string.IsNullOrEmpty(projectId))
-            {
-                BH.Engine.Base.Compute.RecordEvent(new ProjectIDEvent
-                {
-                    Message = "The project ID for this file is now set to " + projectId,
-                    ProjectID = projectId,
-                    FileID = fileID
-                });
-            }
-        }
     }
 }
 
